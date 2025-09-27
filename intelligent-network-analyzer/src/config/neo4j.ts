263b4a6008f9095f8439667738f2574a152f11e6
gartcairn <robert@gartcairn.com>
2025-09-27 15:55:46 +0100

    committed changes

--- intelligent-network-analyzer/src/config/neo4j.ts
+++ intelligent-network-analyzer/src/config/neo4j.ts
@@ -1,8 +1,8 @@
 import { Driver, Session, auth } from 'neo4j-driver';
 
-const uri = 'bolt://localhost:7687';
+const uri = 'neo4j+s://fbc6c6be.databases.neo4j.io';
 const user = 'neo4j';
-const password = 'your_password_here';
+const password = 'zihUCaElZXhpeJgg08pL61m_MR2Si8BOj3tVs_ymPRY';
 
 const driver: Driver = Driver(uri, auth.basic(user, password));
 
@@ -16,3 +16,5 @@ const closeConnection = async (): Promise<void> => {
 };
 
 export { connectToNeo4j, closeConnection };
+
+
